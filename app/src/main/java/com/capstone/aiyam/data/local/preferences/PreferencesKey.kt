7812e100566e818b33967c7dc6df1a4cb8664901
manzil.akbar@gmail.com <manzil.akbar@gmail.com>
2024-11-28 22:11:12 +0700

    small bug fix

--- app/src/main/java/com/capstone/aiyam/data/local/preferences/PreferencesKey.kt
+++ app/src/main/java/com/capstone/aiyam/data/local/preferences/PreferencesKey.kt
@@ -4,5 +4,5 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 
 object PreferencesKey {
     val PUSH_NOTIFICATION_KEY = booleanPreferencesKey("push_notification_key")
-    val EMAIL_NOTIFICATION_KEY = booleanPreferencesKey("push_notification_key")
+    val EMAIL_NOTIFICATION_KEY = booleanPreferencesKey("email_notification_key")
 }
